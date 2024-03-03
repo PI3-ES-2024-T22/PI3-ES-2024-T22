aa6806f1a78e3a6c652c4f643a1ee3b9e62623b0
Guilherme Greco <guilhermem.greco@gmail.com>
2024-03-03 13:08:22 -0300

    Initial commit

---
+++ app/src/main/java/com/example/pi3_es_2024_t22/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.pi3_es_2024_t22
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
