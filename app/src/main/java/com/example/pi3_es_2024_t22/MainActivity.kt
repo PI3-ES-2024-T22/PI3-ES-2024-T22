70c3c448e01e904e3d0f37210535841454992ab5
Guilherme Greco <guilhermem.greco@gmail.com>
2024-03-08 10:19:27 -0300

    feat: Sign up

--- app/src/main/java/com/example/pi3_es_2024_t22/MainActivity.kt
+++ app/src/main/java/com/example/pi3_es_2024_t22/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.pi3_es_2024_t22
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+
+            finish()
+        }, 3000)
     }
 }
